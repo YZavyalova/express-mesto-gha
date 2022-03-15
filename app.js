4519e14f745393c060280d96ce4a7ce07bc1da75
Yaroslavna Zavyalova <YZavyalova2@gmail.com>
2022-03-15 23:55:12 +0500

    sprint13

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use((req, res, next) => {
 
 app.use(usersRouter);
 app.use(cardsRouter);
+app.use((req, res) => res.status(404).send({ message: 'Страница не найдена' }));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}...`);
